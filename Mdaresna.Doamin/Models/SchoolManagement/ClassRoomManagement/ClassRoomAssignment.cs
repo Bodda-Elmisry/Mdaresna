01e5c3f1507d9651a1ebfa920be57113c8cdaf6d
BODDA <elmasry_abdallh@yahoo.com>
2024-03-23 15:42:03 +0200

    Change weekday max length in class room assignment model

--- Mdaresna.Doamin/Models/SchoolManagement/ClassRoomManagement/ClassRoomAssignment.cs
+++ Mdaresna.Doamin/Models/SchoolManagement/ClassRoomManagement/ClassRoomAssignment.cs
@@ -1,4 +1,5 @@
-﻿using Mdaresna.Doamin.Models.SchoolManagement.SchoolManagement;
+﻿using Mdaresna.Doamin.Models.Base;
+using Mdaresna.Doamin.Models.SchoolManagement.SchoolManagement;
 using Mdaresna.Doamin.Models.UserManagement;
 using System;
 using System.Collections.Generic;
@@ -9,31 +10,9 @@ using System.Threading.Tasks;
 
 namespace Mdaresna.Doamin.Models.SchoolManagement.ClassRoomManagement
 {
-    public class ClassRoomAssignment
+    public class ClassRoomAssignment : ClassRoomBaseModel
     {
-        public Guid Id { get; set; }
 
         public DateTime AssignmentDate { get; set; }
-
-        public string WeekDay { get; set; }
-
-        public string Details { get; set; }
-
-        public Guid ClassRoomId { get; set; }
-
-        [ForeignKey(nameof(ClassRoomId))]
-        public ClassRoom ClassRoom { get; set; }
-
-        public Guid SupervisorId { get; set; }
-
-        [ForeignKey(nameof(SupervisorId))]
-        public User Supervisor { get; set; }
-
-        public Guid CourseId { get; set; }
-
-        [ForeignKey(nameof(CourseId))]
-        public SchoolCourse Course { get; set; }
-
-        public decimal Rate { get; set; }
     }
 }
