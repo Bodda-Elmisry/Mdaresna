cc4a701bd577597e1914fecc903eb079282a227d
BODDA <elmasry_abdallh@yahoo.com>
2024-03-29 22:32:57 +0200

    Remove get async from IBaseSharedRepository

--- Mdaresna.Repository/IRepositories/Base/IBaseSharedRepository.cs
+++ Mdaresna.Repository/IRepositories/Base/IBaseSharedRepository.cs
@@ -8,8 +8,6 @@ namespace Mdaresna.Repository.IRepositories.Base
 {
     public interface IBaseSharedRepository<T>
     {
-        Task<T> GetAsync(Guid id);
-
         Task<bool> IsExistAsync(Guid id);
     }
 }
