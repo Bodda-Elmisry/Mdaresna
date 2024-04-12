5e2efeca311850c396c2e5e6f441c438b071f26d
BODDA <elmasry_abdallh@yahoo.com>
2024-04-12 13:29:35 +0200

    Add services injection in program.cs

--- Mdaresna/Program.cs
+++ Mdaresna/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 DependencyInjectionConfig.ConfigerRepositories(builder.Services);
+DependencyInjectionConfig.ConfigerServices(builder.Services);
 
 var app = builder.Build();
 
