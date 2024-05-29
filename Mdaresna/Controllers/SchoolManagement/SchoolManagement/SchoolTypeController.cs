f251794ad37ed5338e808a684f81a4c558c9d02e
BODDA <elmasry_abdallh@yahoo.com>
2024-05-29 14:20:04 +0300

    Get all school types

---
+++ Mdaresna/Controllers/SchoolManagement/SchoolManagement/SchoolTypeController.cs
@@ -0,0 +1,29 @@
+﻿using Mdaresna.Repository.IServices.SchoolManagement.SchoolManagement.Query;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mdaresna.Controllers.SchoolManagement.SchoolManagement
+{
+    [Route("SchoolType")]
+    public class SchoolTypeController : Controller
+    {
+        private readonly ISchoolTypeQueryService schoolTypeQueryService;
+
+        public SchoolTypeController(ISchoolTypeQueryService schoolTypeQueryService)
+        {
+            this.schoolTypeQueryService = schoolTypeQueryService;
+        }
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var result = await schoolTypeQueryService.GetAllAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
