40447538416d1aaf28790e8cefec7651f6924948
Abdallah Elmasry <Abdallah@technomedic.com>
2024-08-09 14:55:18 +0300

    Add create dto

---
+++ Mdaresna/DTOs/SchoolManagementDTO/ClassRoomManagementDTO/CreateClassRoomTeacherCourseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Mdaresna.DTOs.SchoolManagementDTO.ClassRoomManagementDTO
+{
+    public class CreateClassRoomTeacherCourseDTO
+    {
+        public Guid TeacherId { get; set; }
+        public Guid ClassRoomId { get; set; }
+        public Guid CourseId { get; set; }
+    }
+}
